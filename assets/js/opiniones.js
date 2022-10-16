8382a71a37951a3c14ddef8943da4b857fc444f1
AuPedra <pedrazziniaugusto@gmail.com>
2022-10-16 19:25:38 -0300

    MainFinalv1
    Mergeo total al main

--- assets/js/opiniones.js
+++ assets/js/opiniones.js
@@ -10,22 +10,25 @@ let opiniones = JSON.parse( localStorage.getItem('opiniones'));
 function render (eRoot, data) {
     let div = document.createElement('div');
         div.setAttribute('class', 'opinioninfo');
-    let h4 = document.createElement('h3');
-        h4.innerHTML = data.nombre;
-        h4.setAttribute('class', 'nombre')
+    let h3 = document.createElement('h3');
+        h3.innerHTML = data.nombre;
+        h3.setAttribute('class', 'nombre')
     let star = +data.calificacion;
     let div2 = document.createElement('div');
     for (let index = 0; index < star; index++) {
+        let div3 = document.createElement('div')
+            div3.setAttribute('class', 'starimg')
         let img = document.createElement('img');
             img.setAttribute('src', '/assets/icons/star.svg');
-            img.setAttribute('class', 'staricon hide')
-        div2.appendChild(img)
+            img.setAttribute('class', 'staricon')
+        div3.appendChild(img)
+        div2.appendChild(div3)
     }
         div2.setAttribute('class', 'starcontainer')
     let par = document.createElement('p');
         par.innerHTML = data.opinion;
         par.setAttribute('class', 'opinion');
-    div.appendChild(h4);
+    div.appendChild(h3);
     div.appendChild(div2);
     div.appendChild(par);
     eRoot.appendChild(div);
