685237e7214a60d79259e6046c4630bd07b60647
Laura Cabrera <laura.f.cabrera@gmail.com>
2022-10-20 19:51:07 -0300

    form now displays green color visuals upon correct validationn

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,10 +1,17 @@
 const formulario = document.getElementById('form');
-
+// valida campos
 const nameError = document.getElementById('nombre-error');
 const phoneError = document.getElementById('telefono-error');
 const emailError = document.getElementById('email-error');
 const messageError = document.getElementById('mensaje-error');
 const submitError = document.getElementById('submit-error')
+
+// cambia colores
+const nombreValido = document.getElementById('nombre');
+const telefonoValido = document.getElementById('telefono');
+const correoValido = document.getElementById('email');
+const mensajeValido = document.getElementById('textarea');
+
 
 // NOMBRE
 function validateName(){
@@ -13,15 +20,18 @@ function validateName(){
     if (name.length == 0) {
         // imprime en nombre-error
         nameError.innerHTML = 'El campo es requerido.';
+        nombreValido.style.borderColor = 'crimson';
         setTimeout(() => {nameError.innerHTML= ''}, 4000);
         return false;
     }
 
     if (!name.match (/^[A-Za-z]+\s{1}[A-Za-z]+[A-Za-z\s]*$/)){
         nameError.innerHTML = 'Por favor, ingresa tu nombre completo.';
+        nombreValido.style.borderColor = 'crimson';
         return false;
     }
-    nameError.innerHTML = 'valido';
+    nameError.innerHTML = '<i class="fa-regular fa-circle-check valid"></i>';
+    nombreValido.style.borderColor = 'lightseagreen';
     return true;
 }
 
@@ -32,6 +42,7 @@ function validatePhone(){
     if (phone.length == 0) {
         // imprime en telefono-error
         phoneError.innerHTML = 'El campo es requerido.';
+        telefonoValido.style.borderColor = 'crimson';
         setTimeout(() => {phoneError.innerHTML= ''}, 4000);
         return false;
     }
@@ -39,6 +50,7 @@ function validatePhone(){
     if (phone.length <= 8) {
         // imprime en telefono-error
         phoneError.innerHTML = 'El numero de teléfono debe ser de al menos 10 digitos.';
+        telefonoValido.style.borderColor = 'crimson';
         return false;
     }
 
@@ -46,7 +58,8 @@ function validatePhone(){
         phoneError.innerHTML = 'Por favor, ingresa un número de teléfono válido.';
         return false;
     }
-    phoneError.innerHTML = 'valido';
+    phoneError.innerHTML = '<i class="fa-regular fa-circle-check valid"></i>';
+    telefonoValido.style.borderColor = 'lightseagreen';
     return true;
 }
 
@@ -57,15 +70,18 @@ function validateEmail(){
     if (email.length == 0) {
         // imprime en email-error
         emailError.innerHTML = 'El campo es requerido.';
+        correoValido.style.borderColor = 'crimson';
         setTimeout(() => {emailError.innerHTML= ''}, 4000);
 
         return false;
     }
     if (!email.match (/^[A-Za-z0-9_.-]+[@][A-Za-z0-9-]+[A-Za-z0-9-.]+[.]+[a-z]{2,4}$/)) {
         emailError.innerHTML = 'Por favor, ingresa una dirección de correo válida.';
+        correoValido.style.borderColor = 'crimson';
         return false;
     }
-    emailError.innerHTML = 'valido';
+    emailError.innerHTML = '<i class="fa-regular fa-circle-check valid"></i>';
+    correoValido.style.borderColor = 'lightseagreen';
     return true;
 }
 
@@ -79,15 +95,18 @@ function validateMessage(){
     if (message.length == 0) {
         // imprime en mensaje-error
         messageError.innerHTML = 'El campo es requerido.';
+        mensajeValido.style.borderColor = 'crimson';
         setTimeout(() => {messageError.innerHTML= ''}, 4000);
         return false;
     }
     if (missing > 0) {
         messageError.innerHTML = 'Se requieren al menos 30 caracteres.';
+        mensajeValido.style.borderColor = 'crimson';
         return false
 
     }
-    messageError.innerHTML = 'valido';
+    messageError.innerHTML = '<i class="fa-regular fa-circle-check valid"></i>';
+    mensajeValido.style.borderColor = 'lightseagreen';
     return true;
 }
 
@@ -103,6 +122,8 @@ function validateSubmit(){
         return false;
     } else {
         submitError.innerHTML = '¡Enviado correctamente!';
+        submitError.style.color = 'lightseagreen';
+        setTimeout(() => {submitError.innerHTML= ''}, 4000);
     }
 }
 
